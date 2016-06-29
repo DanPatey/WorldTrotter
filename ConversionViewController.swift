52c3a1ee45e7941c206f2e05ba825e27dc30e6c0
Dan Patey <danpatey@danpatey.com>
2016-06-28 21:47:11 -0700

    Updating for silver challenge; background color now changes depending on the time of the day

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -6,4 +6,84 @@
 //  Copyright © 2016 Dan Patey. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
+    
+    let hour = NSCalendar.currentCalendar().component(.Hour, fromDate: NSDate())
+    override func viewWillAppear(animated: Bool) {
+        // If hour is between 06 and 22 light, 23 and 05 it's dark
+        if hour >= 06 && hour <= 22 {
+            print("The hour is:", hour)
+        } else {
+            print("The hour is:", hour)
+            self.view.backgroundColor = UIColor.blackColor()
+        }
+    }
+
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        let replacementStringHasLetters = string.rangeOfCharacterFromSet(NSCharacterSet.letterCharacterSet())
+        
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil{
+            return false
+        } else {
+            return true
+        }
+        
+    }
+}
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Dan Patey on 6/28/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import UIKit
+
+class mapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+    
+}
